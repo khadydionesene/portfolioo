17ceabe306aeeb6dfd3a4a0c704011494521bcad
khadydionesene <senekhadydione@gmail.com>
2026-02-27 16:29:30 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,65 @@
+import streamlit as st
+st.set_page_config(page_title="CV Khady Dione Sene",  layout="wide")
+st.title ("Khady dione sene")
+st.subheader("Geomaticienne")
+with st.sidebar:
+    st.image("image.JPEG",width=200)
+    st.header("CONTACT")
+    st.markdown("adresse:Grand yoff,arafat")
+    st.markdown("Email : senekhadydione@gmail.com")
+    st.markdown("Téléphone : 779875920")
+
+
+
+st.write("""
+Etudiante en 2eme année de bts en geomatique ,dotée d' sens aigu de l' analyse spatiale et d'une excellente maitrise des outils SIG.Serieuse,organisé et capable de travailler aussi bien en autonomie qu en equipe .je suis motivée à contribuer à des projets liés à la cartographie,aux données geographiques et à l amenagement du territoire,tout en renforçant mes compétences pratiques dans un environnement professionel exigeant""")
+
+st.header("FORMATION")
+
+st.markdown( """
+- BTS Géomatique en cours 
+- Licence1 en Mathematiques Physique et Informatique
+- Baccalauréat Scientifique S2
+- Formation bureautique""")
+st.header("COMPÉTENCES TECHNIQUES")
+st.subheader("SIG et cartographie")
+st.write("""
+
+- QGIS
+- ArcGIS
+- Cartographie et Topographie
+- Analyse spatiale""")
+st.subheader("programmation")
+         
+st.write("""
+
+
+- Python (bases et manipulation de données)
+- Anaconda (gestion d’environnement)
+- Streamlit (applications interactives)""")
+st.subheader("Webmapping")
+         
+st.write("""
+
+
+- Leaflet (notions)
+- Intégration de cartes interactives
+- OpenStreetMap """)
+st.subheader("Informatique")
+st.write("""
+
+ 
+- Excel
+- AutoCAD
+- Pack Office
+""")
+st.header("EXPÉRIENCES")
+
+st.markdown("""
+- Collecte de données GPS
+- Traitement et analyse SIG
+- Gestion de projet
+- Travail en équipe et polyvalence
+""")
+
+
